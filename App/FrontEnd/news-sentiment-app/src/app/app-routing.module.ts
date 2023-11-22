6ad0d54fd66f149b9a80df0493d3fd25294f8ab5
stiborv <vasa20017@seznam.cz>
2023-11-22 18:09:26 +0100

    add RouteModule for graph-cosmos

--- App/FrontEnd/news-sentiment-app/src/app/app-routing.module.ts
+++ App/FrontEnd/news-sentiment-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GraphComponent } from './graph/graph.component';
+import { GraphCosmosComponent } from './graph-cosmos/graph-cosmos.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'graph',
     component: GraphComponent
+  },
+  {
+    path: 'graph-cosmos',
+    component: GraphCosmosComponent
   }
 ];
 
