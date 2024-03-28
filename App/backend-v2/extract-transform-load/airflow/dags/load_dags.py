c08745fc2779fac4ac8d881dc269bb9c4d88fad0
stiborv <vasa20017@seznam.cz>
2024-03-28 18:25:44 +0100

    minor updates

--- App/backend-v2/extract-transform-load/airflow/dags/load_dags.py
+++ App/backend-v2/extract-transform-load/airflow/dags/load_dags.py
@@ -1,13 +1,19 @@
-#add additional DAGs folders 
+"""
+This script is used to load the extract transform load DAG in the `extract-transform-load/dag` folder into the Airflow webserver.
+We use the `DagBag` class from the `airflow.models` module to load the DAGs into the global namespace.
+"""
 
 import os
-import sys
 from airflow.models import DagBag
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../../dag/extract'))
 
-dag_bag = DagBag(os.path.join(os.path.dirname(__file__), '../../dag'))
+# Create a DagBag instance
+dag_bag = DagBag(os.path.join(os.path.dirname(__file__), "../../dag"))
 
+# If the DagBag instance is not empty
 if dag_bag:
+    # Iterate over all the DAGs in the DagBag
     for dag_id, dag in dag_bag.dags.items():
+        # Add each DAG to the global namespace
         globals()[dag_id] = dag
+
