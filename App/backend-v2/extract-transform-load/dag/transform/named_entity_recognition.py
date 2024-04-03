8b064a3d74cf98e1030d3d4c8d5e6ff33ff1225e
stiborv <vasa20017@seznam.cz>
2024-04-03 15:47:40 +0200

    refactor transform

---
+++ App/backend-v2/extract-transform-load/dag/transform/named_entity_recognition.py
@@ -0,0 +1,39 @@
+import requests
+import logging
+from airflow.models import TaskInstance
+
+
+NER_MODEL_API_URL = 'http://localhost:5051/extract-entities'
+
+
+def apply_ner(ti: TaskInstance, file_basename:str) -> None:
+    # Pull (file) data from XCom
+    data = ti.xcom_pull(key=f'{file_basename}_json_extract_articles')
+
+    articles_entities = post_request(data)
+
+    logging.info(f"Articles entities {articles_entities}")
+    logging.info(f"pushing into {file_basename}_articles_entities")
+
+    ti.xcom_push(key=f'{file_basename}_articles_entities', value=articles_entities)
+
+def post_request(data): # TODO specify the data type and return type
+    """
+    Request to NER model API.
+    """
+    articles_entities = []
+
+    for article in data:
+        article_content = article['content']
+        
+        try:
+            response = requests.post(NER_MODEL_API_URL, json={'content': article_content})
+            response.raise_for_status()
+
+            entities = response.json()['entities']
+            articles_entities.append(entities)
+        except Exception as e:
+            logging.error(f"Failed post request to NER model. Error: {str(e)}")
+            raise
+
+    return articles_entities
